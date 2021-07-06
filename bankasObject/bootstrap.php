4072c299ee1ed0049f72bf023b5701b590bd257d
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-07-06 20:01:54 +0300

    bt

--- bankasObject/bootstrap.php
+++ bankasObject/bootstrap.php
@@ -4,8 +4,10 @@ defined('ENTER') || die;
 require __DIR__ .'/vendor/autoload.php';
 session_start();
 define('DIR', __DIR__.'/');
-define('URL', 'http://localhost/namu_darbai/bankasObject/public/');
-define('INSTALL_DIR', '/namu_darbai/bankasObject/public/');
+//define('URL', 'http://localhost/namu_darbai/bankasObject/public/');
+define('URL', 'http://eurobankas.com/');
+//define('INSTALL_DIR', '/namu_darbai/bankasObject/public/');
+define('INSTALL_DIR', '/');
 
 function getMessage()
 {
