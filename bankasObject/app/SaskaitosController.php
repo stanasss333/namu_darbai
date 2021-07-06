8199fd792fe5d9b64c9900d419e5220e341cb309
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-07-06 14:22:41 +0300

    ds

--- bankasObject/app/SaskaitosController.php
+++ bankasObject/app/SaskaitosController.php
@@ -14,7 +14,7 @@ class SaskaitosController implements Planas1 {
 
     public function index()
     {
-        return App::view('pagrindinis');
+        return App::view('index', ['saskaitos' => Json::getJson()->showAll()]);
     }
     public function add($id)
     {
@@ -58,7 +58,29 @@ class SaskaitosController implements Planas1 {
 
     public function save()
     {
-        $saskaita = ['id' => rand(10000000, 99999999), 'amount' => 0]; // be garantiju unikalumui
+        $year = rand(0, 99);
+        $newYear = '0'.$year;
+        $goodYear = substr($newYear, -2);
+
+        $month = rand(1, 12);
+        $newMonth = '0'.$month;
+        $goodMonth = substr($newMonth, -2);
+ 
+        if($month % 2 == 0 || $month == 9 || $month == 11){
+            if($month == 2) {
+               $day == rand(1, 28);
+            }
+            $day = rand(1, 30);
+        } 
+        if($month % 2 !== 0 || $month == 8 || $month == 10 || $month == 12) {
+           $day = rand(1, 31);
+        }
+        $newDay = '0'.$day;
+        $goodDay = substr($newDay, -2);
+        
+        $number = 0;
+        $new_number = number_format((float)$number, 2, '.', '');
+        $saskaita =['Vardas' => vardas[rand(0, count($vardas)-1)], 'Pavarde' => pavarde[rand(0, count($pavarde)-1)], 'SaskaitosNr.' => 'LT'. rand(100000000000000000, 999999999999999999), 'AsmensKodas' => '3'. $goodYear. $goodMonth. $goodDay. rand(1000, 9999), 'Suma' => $new_number];
         Json::getJson()->create($saskaita);
         App::redirect();
     }
