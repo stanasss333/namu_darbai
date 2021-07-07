31b43d505b8b0291a66c0d7955ed3262c59f92e4
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-07-07 09:21:06 +0300

    ban

--- bankasObject/app/App.php
+++ bankasObject/app/App.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace Bankas;
-use Plan\Planas;
 
-class App implements Planas {
+class App {
 
     public static function start()
     {
@@ -26,29 +25,30 @@ class App implements Planas {
 
     private static function router()
     {
-        $uri = str_replace(INSTALL_DIR, '', $_SERVER['REQUEST_URI']);
+        //$uri = str_replace(INSTALL_DIR, '', $_SERVER['REQUEST_URI']);
+        $uri = $_SERVER['REQUEST_URI'];
         $uri = explode('/', $uri);
+        array_shift($uri);
 
-        if ($uri[0] == 'testas' && isset($uri[1])) {
-            return (new SaskaitosController)->saskaituTest($uri[1]);
-        }
-        if ($uri[0] === '' && count($uri) === 1) {
-            return (new SaskaitosController)->index();
-        }
-        if ($uri[0] == 'testas' && isset($uri[1])) {
-            return (new LoginController)->loginTest($uri[1]);
-        }
-        if ($uri[0] === '' && count($uri) === 1) {
-            return (new LoginController)->login();
-        }
+        
 
         // 1. Agurku deziu saraso atvaizdavimas GET
-        if (!isset($_GET['action']) && $_SERVER['REQUEST_METHOD'] == 'GET') {
-            self::view('pagrindinis');
+        //if (!isset($_GET['action']) && $_SERVER['REQUEST_METHOD'] == 'GET') {
+          //  self::view('pagrindinis');
+        //}
+        // 6. Naujos dezes pridejimo atvaizdavimas GET
+        if ('prideti-saskaita' == $uri[0]) {
+            if ('GET' == $_SERVER['REQUEST_METHOD']) {
+                
+                //return (new SaskaitosController)->create();
+            }
+            else {
+                // 7. Naujos dezes pridejimo vykdymas POST
+                return (new SaskaitosController)->save();
+            }
         }
 
-        // 2. Pridejimo atvaizdavimas GET
-        
+        // 2. Pridejimo atvaizdavimas GET 
         if ('pridetilesu' == $uri[0] && isset($uri[1])) {
             if ('GET' == $_SERVER['REQUEST_METHOD']) {
                 return (new SaskaitosController)->add($uri[1]);
@@ -71,20 +71,22 @@ class App implements Planas {
             }
         }
 
-        // 6. Naujos dezes pridejimo atvaizdavimas GET
-        if ('prideti-saskaita' == $uri[0]) {
-            if ('GET' == $_SERVER['REQUEST_METHOD']) {
-                return (new SaskaitosController)->create();
-            }
-            else {
-                // 7. Naujos dezes pridejimo vykdymas POST
-                return (new SaskaitosController)->save();
-            }
-        }
-
         // 8. Dezes trynimo vykdymas POST
         if ('delete' == $uri[0] && isset($uri[1]) && 'POST' == $_SERVER['REQUEST_METHOD']) {
             return (new SaskaitosController)->delete($uri[1]);
+        }
+
+        if ($uri[0] == 'testas' && isset($uri[1])) {
+            return (new SaskaitosController)->saskaituTest($uri[1]);
+        }
+        if ($uri[0] === '' && count($uri) === 1) {
+            return (new SaskaitosController)->index();
+        }
+        if ($uri[0] == 'testas' && isset($uri[1])) {
+            return (new LoginController)->loginTest($uri[1]);
+        }
+        if ($uri[0] === '' && count($uri) === 1) {
+            return (new LoginController)->login();
         }
        
         self::view('404');
